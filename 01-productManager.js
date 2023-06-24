9dc7e0bd7773079bd86422abdc1de7e32d43b277
Fernando Breme <ferchubreme123@gmail.com>
2023-06-23 22:47:50 -0300

    segundo desafio

--- 01-productManager.js
+++ 01-productManager.js
@@ -1,14 +1,32 @@
- // CREACION DE LA CLASS
+const fs = require("fs");
+
+// CREACION DE LA CLASS
 class ProductManager{
-        constructor(){
+        constructor(path){
             this.products = []
             this.nextId = 1
+            this.path = path;
         }
+        //OBTENER INFO DEL ARRAY
 
+        async getProducts(){
+            try {
+                if(fs.existsSync(this.path)){
+                    const productsFile = await fs.promises.readFile(this.path, "utf-8");
+                    const products = JSON.parse(productsFile);
+                    return products
+                } else{
+                    return this.products
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
 // METODO PARA CREAR EL PRODUCTO CON LOS PARAMETROS REQUERIDOS
-        addProduct(code, title, description, thumbnail, price, stock  ){
-
-            //VALIDACION DE CAMPOS
+        async addProduct(code, title, description, thumbnail, price, stock  ){
+            try {
+                const productsFile = await this.getProducts();
+                            //VALIDACION DE CAMPOS
             
             if(!code || !title || !description || !thumbnail || !price || !stock){
                 console.log(`Todos los campos son obligatorios!`)
@@ -16,9 +34,12 @@ class ProductManager{
             }
             
             //VALIDACION DE CODIGO DUPLICADO
+            const  verifyCode =  async (code) => await productsFile.some(product => product.code === code)
+        
             
-            if(this.verifyCode(code)){
+            if( await verifyCode(code)){
                 console.log(`El codigo Ingresado ya existe!`)
+                return
             }
 
             //DATOS DEL PRODUCTO
@@ -35,50 +56,116 @@ class ProductManager{
             
             //AGREGAR PRODUCTO NUEVO AL ARRAY
 
-            this.products.push(product);
+            productsFile.push(product);
+            await fs.promises.writeFile(this.path, JSON.stringify(productsFile));
             this.nextId++
-        }
-        
+            } 
+            catch (error) {
+                console.log(error);
+            }
+
         //METODO PARA VERIFICAR CODIGO INGRESADO
         
-        verifyCode(code){
-            return this.products.some(product => product.code === code)
-        }
 
-        //OBTENER INFO DEL ARRAY
-
-        getProducts(){
-            return this.products
         }
+        
+
+
+
 
         //METODO PARA FILTRAR PRODUCTO POR ID
 
-        getProductById(id){
-            const product = this.products.find(product => product.id === id);
+        async getProductById(id){
+            try {
+                const productsFile = await this.getProducts();
+                const product = productsFile.find(product => product.id === id);
 
-            if(product){
-                return product;
-            } else{
-                console.log(`404 Not Found`);
-                        }
+                if(product){
+                    return product;
+                } else{
+                    console.log(`404 Not Found`);
+                            }
+            } catch (error) {
+                console.log({message: "error"});
+            }
+
+        }
+
+        // MOODIFICAR DATOS DE UN PRODUCTO
+
+        async updateProd (updated, prodId){
+            try {
+                const products = await this.getProducts();
+                const index = products.findIndex(prod => prod.id === prodId);
+                
+                if(index != -1){
+                    products[index] = {...updated, ...products[index]}
+                    await fs.promises.writeFile(this.path, JSON.stringify(products));
+                    console.log (`product modified`);
+
+                     } else{
+                        console.log("product not found");
+                     }
+
+            } catch (error) {
+                console.log(error)
+            }
+
+        }
+
+        // ELIMINAR UN PRODUCTO
+
+        async deleteProd(id){
+
+            try {
+                const products = await this.getProducts();
+                const prodIndex = products.findIndex(prod => prod.id === id);
+                if(prodIndex != -1){
+                    products.splice(prodIndex, 1);
+                    await fs.promises.writeFile(this.path, JSON.stringify(products));
+                    console.log("product deleted.")
+                    return
+                } else{
+                    console.log("product not found");
+                    
+                } 
+            } catch (error) {
+                console.log(error);
+            }
+
         }
     }
 //
-const manager = new ProductManager();
+const manager = new ProductManager("./products.json");
 
-// PRODUCTO AGREGADO CON EXITO (2)
+const test = async ()=>{
+    // PRODUCTO AGREGADO CON EXITO (2)
 
-manager.addProduct(`CO001`, `Anillo`, `Anillo de plata con grabado interno`, `Imagen no encontrada`, `$3500`, `10 unidades`);
-manager.addProduct(`CO002`, `reloj`, `Reloj pulsera con tiras de jean y ebilla de 1cm`, `imagen1.jpg`, `$4500`, `5 unidades`);
+    await manager.addProduct(`CO001`, `Anillo`, `Anillo de plata con grabado interno`, `Imagen no encontrada`, `$3500`, `10 unidades`);
+    await manager.addProduct(`CO002`, `reloj`, `Reloj pulsera con tiras de jean y ebilla de 1cm`, `imagen1.jpg`, `$4500`, `5 unidades`);
 
-console.log(manager.getProducts());
-console.log(manager.getProductById(1));
+    const products = await manager.getProducts();
+    console.log(products);
 
-//***PRODUCTO RECHAZADO POR FALTA DE CAMPOS***
+    const prodId = await manager.getProductById(1);
+    console.log(prodId);
 
-console.log(manager.addProduct(`CO003`, `Anillo2`, `Un nuevo Anillo`, `URL`, `$3800`));
+    //***PRODUCTO RECHAZADO POR FALTA DE CAMPOS***
 
-//***PRODUCTO RECHAZADO POR CODIGO DUPLICADO***
+    console.log( await manager.addProduct(`CO003`, `Anillo2`, `Un nuevo Anillo`, `URL`, `$3800`));
 
-console.log(manager.addProduct(`CO002`, `reloj2`, `Reloj nuevo`, `imagen2.jpg`, `$4500`, `3 unidades`));
+    //***PRODUCTO RECHAZADO POR CODIGO DUPLICADO***
+
+    console.log(await manager.addProduct(`CO002`, `reloj2`, `Reloj nuevo`, `imagen2.jpg`, `$4500`, `3 unidades`));
+
+    //***  PRODUCTO ELIMINADO  ****/
+
+
+   // console.log(await manager.deleteProd(1));
+
+   // const products2 = await manager.getProducts();
+    //console.log(products2);
+}
+
+test();
 
